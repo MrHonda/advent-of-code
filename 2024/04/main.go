73244b6d4064e808993ffc47ed0e9aa86ce8f6cb
MrHonda <hondacze@gmail.com>
2024-12-04 18:50:25 +0100

    [2024-04_P1] https://adventofcode.com/2024/day/4

---
+++ 2024/04/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	handleError(err)
+
+	part1(parseData(data))
+}
+
+func part1(data []string) {
+	horizontalCount := countAll(data)
+	verticalCount := countAll(getVertical(data))
+	diagonalCount := countAll(getDiagonal(data))
+	result := horizontalCount + verticalCount + diagonalCount
+
+	fmt.Println("part1:", result, "| horizontal:", horizontalCount, "| vertical:", verticalCount, "| diagonal:", diagonalCount)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func countAll(lines []string) int {
+	r1 := regexp.MustCompile("XMAS")
+	r2 := regexp.MustCompile("SAMX")
+	count := 0
+
+	for _, line := range lines {
+		count += len(r1.FindAllStringIndex(line, -1)) + len(r2.FindAllStringIndex(line, -1))
+	}
+
+	return count
+}
+
+func parseData(data []byte) []string {
+	result := []string{}
+
+	lines := strings.Split(string(data), "\n")
+
+	for _, line := range lines {
+		result = append(result, strings.ReplaceAll(line, "\r", ""))
+	}
+
+	return result
+}
+
+func getVertical(lines []string) []string {
+	result := []string{}
+
+	for i := range len(lines[0]) {
+		vertLine := ""
+		for _, line := range lines {
+			vertLine += string(line[i])
+		}
+
+		result = append(result, vertLine)
+	}
+
+	return result
+}
+
+func getDiagonal(lines []string) []string {
+	const maxLen = 4
+	r1 := regexp.MustCompile("XMAS")
+	r2 := regexp.MustCompile("SAMX")
+
+	result := []string{}
+
+	for i, line := range lines {
+		// left to right
+		for j := range len(line) {
+			diagLine := ""
+			for k := 0; k < len(lines); k++ {
+				row := k + i
+				col := j + k
+
+				if col >= len(line) || row >= len(lines) {
+					break
+				}
+
+				diagLine += string(lines[row][col])
+			}
+
+			if len(diagLine) < maxLen {
+				break
+			}
+
+			if r1.MatchString(diagLine) || r2.MatchString(diagLine) {
+				fmt.Println(diagLine, "left", "i:", i, "j:", j)
+			}
+
+			result = append(result, diagLine)
+
+			//do full construction only on the first line
+			//on the other lines, do just FIRST columns
+			if i >= 1 {
+				break
+			}
+		}
+
+		// right to left
+		for j := len(line) - 1; j >= 0; j-- {
+			diagLine := ""
+			for k := 0; k < len(lines); k++ {
+				row := k + i
+				col := j - k
+
+				if col < 0 || row >= len(lines) {
+					break
+				}
+
+				diagLine += string(lines[row][col])
+			}
+
+			if len(diagLine) < maxLen {
+				break
+			}
+
+			if r1.MatchString(diagLine) || r2.MatchString(diagLine) {
+				fmt.Println(diagLine, "right", "i:", i, "j:", j)
+			}
+
+			result = append(result, diagLine)
+
+			//do full construction only on the first line
+			//on the other lines, do just LAST columns
+			if i >= 1 {
+				break
+			}
+		}
+	}
+
+	return result
+}
