f2c8add3fef44cdb29387de8fc84bc8fff760395
MrHonda <hondacze@gmail.com>
2024-12-06 18:55:41 +0100

    [2024-06_P2] Slow https://adventofcode.com/2024/day/6

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -23,18 +24,93 @@ func main() {
 	handleError(err)
 
 	part1(parseData(string(data)))
+	part2(parseData(string(data)))
 }
 
 func part1(grid [][]string) {
+	visited, _ := getVisited(grid)
+
+	fmt.Println("part1", len(visited))
+}
+
+func part2(grid [][]string) {
+	result := 0
+	visited, _ := getVisited(grid)
+	startI, startJ := getGridCurrentPos(grid)
+
+	for i, visitVal := range visited {
+		values := strings.Split(visitVal, ",")
+		valI, err1 := strconv.Atoi(values[0])
+		handleError(err1)
+
+		valJ, err2 := strconv.Atoi(values[1])
+		handleError(err2)
+
+		if (startI == valI && startJ == valJ) || grid[valI][valJ] == block {
+			continue
+		}
+
+		gridVal := grid[valI][valJ]
+		grid[valI][valJ] = block
+		_, loop := getVisited(grid)
+		grid[valI][valJ] = gridVal
+
+		if loop {
+			result++
+		}
+
+		println(i, len(visited))
+
+	}
+
+	fmt.Println("part2", result)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func parseData(data string) [][]string {
+	lines := strings.Split(string(data), "\r\n")
+	grid := [][]string{}
+
+	for _, line := range lines {
+		grid = append(grid, strings.Split(line, ""))
+	}
+
+	return grid
+}
+
+func getGridCurrentPos(grid [][]string) (int, int) {
+	for i := range grid {
+		for j := range grid[i] {
+			if slices.Contains(dirs, grid[i][j]) {
+				return i, j
+			}
+
+		}
+	}
+
+	panic("current position not found")
+}
+
+func getVisited(grid [][]string) ([]string, bool) {
 	visited := []string{}
 	maxI := len(grid) - 1
 	maxJ := len(grid[0]) - 1
 	i, j := getGridCurrentPos(grid)
 	dir := grid[i][j]
-
+	tries := len(grid) * len(grid[0])
+	try := 0
 loop:
 	for {
-		currentVisit := string(i) + "," + string(j)
+		if try >= tries {
+			return nil, true
+		}
+
+		currentVisit := strconv.Itoa(i) + "," + strconv.Itoa(j)
 
 		if !slices.Contains(visited, currentVisit) {
 			visited = append(visited, currentVisit)
@@ -88,37 +164,10 @@ loop:
 		default:
 			panic("unkown dir" + dir)
 		}
+
+		try++
+		// println(try, tries)
 	}
 
-	fmt.Println("part1", len(visited))
-}
-
-func handleError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func parseData(data string) [][]string {
-	lines := strings.Split(string(data), "\r\n")
-	grid := [][]string{}
-
-	for _, line := range lines {
-		grid = append(grid, strings.Split(line, ""))
-	}
-
-	return grid
-}
-
-func getGridCurrentPos(grid [][]string) (int, int) {
-	for i := range grid {
-		for j := range grid[i] {
-			if slices.Contains(dirs, grid[i][j]) {
-				return i, j
-			}
-
-		}
-	}
-
-	panic("current position not found")
+	return visited, false
 }
