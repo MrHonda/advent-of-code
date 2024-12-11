4174147c375a5fbbf05af8b80979f0a3a6f43ba5
MrHonda <hondacze@gmail.com>
2024-12-11 17:17:20 +0100

    [2024-11_P2] https://adventofcode.com/2024/day/11

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -15,6 +15,7 @@ func main() {
 	handleError(err)
 
 	part1(parseData(string(stones)))
+	part2(parseData(string(stones)))
 }
 
 func part1(stones []string) {
@@ -23,6 +24,24 @@ func part1(stones []string) {
 	}
 
 	fmt.Println("part1", len(stones))
+}
+
+func part2(stones []string) {
+	stoneCounts := make(map[string]int)
+	for _, stone := range stones {
+		stoneCounts[stone]++
+	}
+
+	for i := 0; i < 75; i++ {
+		stoneCounts = blink2(stoneCounts)
+	}
+
+	result := 0
+	for _, count := range stoneCounts {
+		result += count
+	}
+
+	fmt.Println("part2", result)
 }
 
 func handleError(err error) {
@@ -38,8 +57,6 @@ func parseData(data string) []string {
 func blink(stones []string) []string {
 	for i := 0; i < len(stones); i++ {
 		stone := stones[i]
-		stoneInt, err := strconv.Atoi(stone)
-		handleError(err)
 
 		if stone == "0" {
 			stones[i] = "1"
@@ -56,9 +73,38 @@ func blink(stones []string) []string {
 			stones = slices.Insert(stones, i+1, stone2)
 			i++
 		} else {
+			stoneInt, err := strconv.Atoi(stone)
+			handleError(err)
 			stones[i] = strconv.Itoa(stoneInt * 2024)
 		}
 	}
 
 	return stones
+}
+
+func blink2(stones map[string]int) map[string]int {
+	newStones := map[string]int{}
+
+	for stone, count := range stones {
+		if stone == "0" {
+			newStones["1"] += count
+		} else if len(stone)%2 == 0 {
+			stone1 := stone[:(len(stone) / 2)]
+			stone2 := stone[(len(stone) / 2):]
+			stone2 = strings.TrimLeft(stone2, "0")
+
+			if stone2 == "" {
+				stone2 = "0"
+			}
+
+			newStones[stone1] += count
+			newStones[stone2] += count
+		} else {
+			stoneInt, err := strconv.Atoi(stone)
+			handleError(err)
+			newStones[strconv.Itoa(stoneInt*2024)] += count
+		}
+	}
+
+	return newStones
 }
